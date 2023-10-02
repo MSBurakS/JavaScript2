3a8433747106f840d141e7add206f2cd60478b9b
burak <msburaks@gmail.com>
2023-10-02 14:05:14 +0300

    add app.js props

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10)*3/2-2;  
+
+let calulationDescription =`( ${defaultResult}+10)*3/2-2`;
+
+outputResult(currentResult,calulationDescription);
