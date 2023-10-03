1b44209b7572e174a4c0705985f369f8cca158f6
burak <msburaks@gmail.com>
2023-10-03 16:24:40 +0300

    add function props

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+}
+
 function add(){
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult,'');
+    const entredNumber = getUserNumberInput();
+    const calcDesciription = `${currentResult} + ${entredNumber}`
+    currentResult = currentResult + entredNumber;
+    outputResult(currentResult,calcDesciription);
 }
 
 addBtn.addEventListener('click',add);
